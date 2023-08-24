f25d9f2ee4e48ddd0f58abfc9d932e18e4b14809
Ignacio Fuentes <ignacio.fuentes@genesys.cl>
2023-08-24 15:56:55 -0400

    test

---
+++ test/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LectorQR.Startup))]
+namespace LectorQR
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
